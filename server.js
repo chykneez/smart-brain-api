9e1712272da362a15c79fd8a32dd21e877f73d08
Patrick Nguyen <iamtiredofyou2@gmail.com>
2021-02-23 10:16:48 -0500

    Created a mock database object for testing

--- server.js
+++ server.js
@@ -2,12 +2,34 @@ const express = require('express');
 
 const app = express();
 
+// Testing
+const db = {
+  users: [
+    {
+      id: '1',
+      name: 'Patrick',
+      email: 'patrick@gmail.com',
+      password: 'password',
+      entries: 0,
+      createdAt: new Date(),
+    },
+    {
+      id: '2',
+      name: 'Daniela',
+      email: 'daniela@gmail.com',
+      password: 'password',
+      entries: 0,
+      createdAt: new Date(),
+    },
+  ],
+};
+
 app.get('/', (req, res) => {
   res.send('This is working!');
 });
 
 app.post('/login', (req, res) => {
-  res.send('Logging in');
+  res.json('Logging in');
 });
 
 app.listen(3000, () => {
