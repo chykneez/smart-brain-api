a9ceaf8ba5d91f9d090745577eb2ecf1c12b906e
Patrick Nguyen <iamtiredofyou2@gmail.com>
2021-02-23 15:08:18 -0500

    Fixed the login endpoint

--- server.js
+++ server.js
@@ -58,14 +58,28 @@ app.post('/register', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-  if (
-    req.body.email === db.users[0].email &&
-    req.body.password === db.users[0].password
-  )
-    res.json(db.users[0]);
-  else {
-    res.status(400).json('Incorrect email or password!');
-  }
+  db.select('email', 'hash')
+    .from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+
+      if (isValid) {
+        return db
+          .select('*')
+          .from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            res.json(user[0]);
+          })
+          .catch(err => res.status(400).json('User does not exist!'));
+      } else {
+        res.status(400).json('Incorrect email or password! Please try again!');
+      }
+    })
+    .catch(err =>
+      res.status(400).json('Incorrect email or password! Please try again!')
+    );
 });
 
 app.get('/profile/:id', (req, res) => {
