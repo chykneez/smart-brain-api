e7f11923ed671767127ddb2c192d4b96e3485e15
Patrick Nguyen <iamtiredofyou2@gmail.com>
2021-02-23 10:13:43 -0500

    Created a mock login route

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('This is working!');
+});
+
+app.post('/login', (req, res) => {
+  res.send('Logging in');
+});
+
 app.listen(3000, () => {
   console.log('Listening on port 3000...');
 });
