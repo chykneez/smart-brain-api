0f47d81e2ba18733b9906c1e8f2ae7fdb98523cd
Patrick Nguyen <iamtiredofyou2@gmail.com>
2021-02-25 10:34:07 -0500

    Fixed some login/register security issues

--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,9 @@
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
 
+  if (!email || !name || !password)
+    return res.status(400).json('Please fill out all of the fields!');
+
   const hash = bcrypt.hashSync(password);
 
   db.transaction(trx => {
