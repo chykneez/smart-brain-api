a6d8a3308fc098293f464e97e646ba09846f94bd
Patrick Nguyen <iamtiredofyou2@gmail.com>
2021-02-25 09:59:12 -0500

    Refactored the register endpoint callback function

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+const register = require('./controllers/register');
+
 const db = knex({
   client: 'pg',
   connection: {
@@ -23,38 +25,7 @@ app.get('/', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-  const { email, name, password } = req.body;
-
-  const hash = bcrypt.hashSync(password);
-
-  db.transaction(trx => {
-    trx
-      .insert({
-        hash,
-        email,
-      })
-      .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users')
-          .returning('*')
-          .insert({
-            name,
-            email: loginEmail[0],
-            createdAt: new Date(),
-          })
-          .then(user => {
-            res.json(user[0]);
-          })
-          .then(trx.commit)
-          .catch(trx.rollback);
-      })
-      .catch(err =>
-        res
-          .status(400)
-          .json('That email is already taken! Please use another one!')
-      );
-  });
+  register.handleRegister(req, res, db, bcrypt);
 });
 
 app.post('/login', (req, res) => {
