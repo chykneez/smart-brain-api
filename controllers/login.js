29af739709ced3c166cefe21e894b967e357aaf3
Patrick Nguyen <iamtiredofyou2@gmail.com>
2021-02-25 10:03:14 -0500

    Refactored the login endpoint callback function

---
+++ controllers/login.js
@@ -0,0 +1,28 @@
+const handleLogin = (req, res, db, bcrypt) => {
+  db.select('email', 'hash')
+    .from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+
+      if (isValid) {
+        return db
+          .select('*')
+          .from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            res.json(user[0]);
+          })
+          .catch(err => res.status(400).json('User does not exist!'));
+      } else {
+        res.status(400).json('Incorrect email or password! Please try again!');
+      }
+    })
+    .catch(err =>
+      res.status(400).json('Incorrect email or password! Please try again!')
+    );
+};
+
+module.exports = {
+  handleLogin: handleLogin,
+};
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const knex = require('knex');
 
 const register = require('./controllers/register');
+const login = require('./controllers/login');
 
 const db = knex({
   client: 'pg',
@@ -29,28 +30,7 @@ app.post('/register', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-  db.select('email', 'hash')
-    .from('login')
-    .where('email', '=', req.body.email)
-    .then(data => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-
-      if (isValid) {
-        return db
-          .select('*')
-          .from('users')
-          .where('email', '=', req.body.email)
-          .then(user => {
-            res.json(user[0]);
-          })
-          .catch(err => res.status(400).json('User does not exist!'));
-      } else {
-        res.status(400).json('Incorrect email or password! Please try again!');
-      }
-    })
-    .catch(err =>
-      res.status(400).json('Incorrect email or password! Please try again!')
-    );
+  login.handleLogin(req, res, db, bcrypt);
 });
 
 app.get('/profile/:id', (req, res) => {
